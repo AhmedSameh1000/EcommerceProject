804afc6411f40e1df1adf0adb6dc6df5bb10d088
Ahmed Sameh <ahmeds14@protonmail.ch>
2024-01-24 12:46:50 +0200

    SomeChanges

--- Api/InfraStructure/obj/Debug/net7.0/InfraStructure.AssemblyInfo.cs
+++ Api/InfraStructure/obj/Debug/net7.0/InfraStructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InfraStructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ea79510c76192ba158ce9528486373af0de130")]
 [assembly: System.Reflection.AssemblyProductAttribute("InfraStructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InfraStructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
